d73c31f5e665e3e5976c0fd55cb60afb5101cd89
Your Name <you@example.com>
2024-02-23 08:12:19 +0700

    third push

--- resources/js/Pages/User/CreateUser.vue
+++ resources/js/Pages/User/CreateUser.vue
@@ -1,99 +1,214 @@
 <script setup>
-import { useForm } from '@inertiajs/vue3';
+import { useForm } from "@inertiajs/vue3";
 
-  const form = useForm({
-    name: '',
-    gender:'',
-    email:'',
-    age:'',
-    no_reg:'',
-    no_rawat:'',
-    
-    unit:'',
-    dr:''
+const form = useForm({
+    name: "",
+    gender: "",
+    email: "",
+    age: "",
+    no_reg: "",
+    no_rawat: "",
 
-  })
-
-
+    unit: "",
+    dr: "",
+});
 </script>
 
-<template >
-  <h1 class="font-bold text-2xl p-4">Input Patient Data</h1>
-  <form @submit.prevent="form.post('/users')" >
-    <div class=" flex flex-col  m-auto min-h-screen items-center px-16 py-10    ">
+<template>
+    <h1 class="font-bold text-2xl p-4">Input Patient Data</h1>
+    <form @submit.prevent="form.post('/users')">
+        <div class="flex flex-col m-auto min-h-screen px-12 py-10">
+            <div class=" py-2">
+                <label
+                    class="flex flex-row text-white font-bold md:text-left mb-1 md:mb-0 pr-4"
+                    for="inline-full-name"
+                >
+                <div class="pr-[2.2rem] ">
 
-        <label class="form-control  w-3/4 ">
-  <div class="label">
-    <span class="label-text">Name</span>
-    
-  </div>
-  <input type="text" placeholder="Add Name" v-model="form.name" class="input input-bordered text-black w-full rounded-md" />
- 
-</label>
-<label class="form-control  w-3/4 ">
-  <div class="label">
-    <span class="label-text">Email</span>
-    
-  </div>
-  <input type="email" placeholder="Add Email" v-model="form.email" class="input input-bordered text-black w-full rounded-md" />
- 
-</label>
-<label class="form-control  w-3/4 ">
-  <div class="label">
-    <span class="label-text">Gender</span>
-    
-  </div>
-  <input type="text" placeholder="Male or Femal" v-model="form.gender" class="input input-bordered  text-black w-full rounded-md " />
- 
-</label>
-<label class="form-control w-3/4 ">
-  <div class="label">
-    <span class="label-text">Age</span>
-    
-  </div>
-  <input type="text" placeholder="Input Age" v-model="form.age" class="input input-bordered  text-black w-full rounded-md" />
- 
-</label>
-<label class="form-control  w-3/4 ">
-  <div class="label">
-    <span class="label-text">Registration Number</span>
-    
-  </div>
-  <input type="text" placeholder="Regist Number" v-model="form.no_reg" class="input input-bordered  text-black w-full rounded-md" />
- 
-</label>
-<label class="form-control  w-3/4 ">
-  <div class="label">
-    <span class="label-text">Care Number</span>
-    
-  </div>
-  <input type="text" placeholder="Care Number" v-model="form.no_rawat" class="input input-bordered  text-black w-full rounded-md" />
- 
-</label>
-<label class="form-control w-3/4 ">
-  <div class="label">
-    <span class="label-text">Unit</span>
-    
-  </div>
-  <input type="text" placeholder="Input Unit" v-model="form.unit" class="input input-bordered  text-black w-full rounded-md" />
- 
-</label>
-<label class="form-control  w-3/4 ">
-  <div class="label">
-    <span class="label-text">Doctor</span>
-    
-  </div>
-  <input type="text" placeholder="Add Doctor" v-model="form.dr" class="input input-bordered  text-black w-full rounded-md" />
- 
-</label>
-<div class="py-2">
+                  Full Name
+                </div>
+                    <input
+                    type="text"
+                    placeholder="Add Name (ex: Iqbal Trikiswoyo)"
+                    v-model="form.name"
+                    class="input input-bordered text-black w-full mx-2 rounded-md"
+                />
+                </label>
+            </div>
+            <div class=" py-2">
+                <label
+                    class="flex flex-row text-white font-bold md:text-left mb-1 md:mb-0 pr-4"
+                    for="inline-full-name"
+                >
+                <div class="pr-[4rem] py-2">
 
-</div>
-<input type="submit" value="Add Patient" class="w-32 h-12 bg-blue-500 hover:bg-blue-700 hover:cursor-pointer text-white m-10"/>
-</div>
-      </form>
+                  Email
+                </div>
+                    <input
+                    type="email"
+                    placeholder="Add Email (ex: iqbal@gmail.com) "
+                    v-model="form.email"
+                    class="input input-bordered text-black w-1/2 mx-2 rounded-md"
+                />
+                </label>
+            </div>
+            <div class="py-2">
+                <label
+                    class="flex flex-row text-white font-bold md:text-left mb-1 md:mb-0 pr-4"
+                    for="inline-full-name"
+                >
+                <div class="pr-[3.2rem] py-2">
+
+                  Gender
+                </div>
+                    <select
+                    
+                    placeholder="Male or Female"
+                    v-model="form.gender"
+                    class="bg-white text-black w-1/3 mx-2 rounded-md"
+                 >
+                 <option  value="Male" selected>Male</option>
+                 <option value="Female">Female</option>
+                </select>
+                </label>
+            </div>
+            <div class=" py-2">
+                <label
+                    class="flex flex-row text-white font-bold md:text-right mb-1 md:mb-0 pr-4"
+                    for="inline-full-name"
+                >
+                <div class="pr-[4.8rem] py-2">
+
+                  Age
+                </div>
+                    <input
+                    type="text"
+                    placeholder="Add Age (max 100)"
+                    v-model="form.age"
+                    class="input input-bordered text-black w-1/4 mx-2 rounded-md"
+                />
+                </label>
+            </div>
+            <div class=" py-2">
+                <label
+                    class="flex flex-row text-white font-bold md:text-right mb-1 md:mb-0 pr-4"
+                    for="inline-full-name"
+                >
+                <div class="pr-4 py-2">
+
+                  Reg Number
+                </div>
+                    <input
+                    type="text"
+                    placeholder="Add Registration Number (max 7 character)"
+                    v-model="form.no_reg"
+                    class="input input-bordered text-black w-9/12 mx-2 rounded-md"
+                    maxlength="7"
+                />
+                </label>
+            </div>
+            <div class=" py-2">
+                <label
+                    class="flex flex-row text-white font-bold text-left mb-1 md:mb-0 pr-4"
+                    for="inline-full-name"
+                >
+                <div class="py-2 pr-3">
+
+                  Care Number
+                </div>
+              
+                    <input
+                    type="text"
+                    placeholder="Add Care Number (max 4 Character)"
+                    v-model="form.no_rawat"
+                    class="input input-bordered text-black w-2/4 mx-2  rounded-md"
+                    maxlength="4"
+                />
+                </label>
+            </div>
+            <div class=" py-2">
+                <label
+                    class="flex flex-row text-white font-bold text-left mb-1 md:mb-0 pr-4"
+                    for="inline-full-name"
+                >
+                <div class="pr-20">
+
+                  Unit
+                </div>
+                    <select
+                    type="text"
+                    placeholder="Add Unit (ex: Kelamin)"
+                    v-model="form.unit"
+                    class="bg-white text-black w-3/4 mx-2 rounded-md"
+                > 
+                <option value="Radiologi">Radiologi</option>
+                <option value="Fisioterapi">Fisioterapi</option>
+                <option value="THT">THT</option>
+                <option value="Kandungan">Kandungan</option>
+                <option value="UGD">UGD</option>
+                <option value="IGD">IGD</option>
+                <option value="ICU">ICU</option>
+                <option value="Poli Jantung"> Poli Jantung</option>
+                <option value="Poli Anak">Poli Anak</option>
+                <option value="Poli Gigi">Poli Gigi</option>
+                <option value="Poli Mata">Poli Mata</option>
+                <option value="Poli Kulit">Poli Kulit</option>
+                <option value="Poli Saraf">Poli Saraf</option>
+                <option value="Poli Penyakit Dalam">Poli Penyakit dalam</option>
+                <option value="Poli Kelamin">Poli Kelamin</option>
+                <option value="Poli Jiwa">Poli Jiwa</option>
+                <option value="Poli Gizi">Poli Gizi</option>
+                <option value="Poli Bedah Mulut">Poli Bedah Mulut</option>
+                <option value="Poli Onkologi Radiasi">Poli Onkologi Radiasi</option>
 
 
+              </select>
+                </label>
+            </div>
+            <div class=" py-2">
+                <label
+                    class="flex flex-row text-white font-bold md:text-right mb-1 md:mb-0 pr-4"
+                    for="inline-full-name"
+                >
+                <div class="pr-[3.8rem] py-2">
 
+                  Doctor
+                </div>
+                    <select
+                    
+                    placeholder="Add Doctor (Ex: Dr. Zakir Naik)"
+                    v-model="form.dr"
+                    class="bg-white text-black w-3/4 mx-2 rounded-md"
+                > 
+                <option value="Dr. Reza Oktavian">Dr. Reza Oktavian</option>
+                <option value="Dr. Miranda">Dr. Miranda</option>
+                <option value="Dr. Alcina Dimitrescu">Dr. Alcina Dimitrescu</option>
+                <option value="Dr. Donna Beneviento">Dr. Donna Beneviento</option>
+                <option value="Dr. William Lu">Dr. William Lu</option>
+                <option value="Dr. Alex Turner">Dr. Alex Turner</option>
+                <option value="Dr. Jeffrey Epstein">Dr. Jeffrey Epstein</option>
+                <option value="Dr. Pablo Escobar">Dr. Pablo Escobar</option>
+                <option value="Dr. Eimi Fukada">Dr. Eimi Fukada</option>
+                <option value="Dr. Ambatukam">Dr. Ambatukam</option>
+                <option value="Dr. Luis Asmoking">Dr. Luis Asmoking</option>
+                <option value="Dr. Ashley Graham">Dr. Ashley Graham</option>
+                <option value="Dr. William Birkin">Dr. William Birkin</option>
+                <option value="Dr. Sherry Birkin">Dr. Sherry Birkin</option>
+                <option value="Dr. Annete Birkin">Dr. Annete Birkin</option>
+                <option value="Dr. Ozwell E. Spencer">Dr. Ozwell E. Spencer</option>
+                <option value="Dr. Albert Wesker">Dr. Albert Wesker</option>
+                <option value="Dr. Karl Heisenberg">Dr. Karl Heisenberg</option>
 
+              </select>
+                </label>
+            </div>
+            
+
+            <input
+                type="submit"
+                value="Add Patient"
+                class="w-32 h-12  bg-blue-500 hover:bg-blue-700 hover:cursor-pointer text-white my-10"
+            />
+        </div>
+    </form>
 </template>
--- resources/js/Pages/User/ListUser.vue
+++ resources/js/Pages/User/ListUser.vue
@@ -1,71 +1,73 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Link } from "@inertiajs/vue3";
 defineProps({
     users: Object,
-})
-
-
+});
 </script>
 
-
 <template>
-    <div class="max-w-8xl  p-6 items-center  ">
-      <div class="flex flex-row ">
+    <div class="max-w-8xl p-6 items-center">
+        <div class="flex flex-row">
+            <h1 class="p-2 text-2xl font-bold">Patient Data</h1>
+            <a href="/users/create">
+                <button
+                    class="bg-green-500 text-white w-32 h-8 my-2 mx-4 rounded-md hover:bg-green-700"
+                >
+                   ➕ Add Patient
+                </button>
+            </a>
+        </div>
 
-        <h1 class="p-2 text-2xl font-bold">Patient Data</h1>
-        <a href="/users/create">
+        <div class="overflow-x-auto">
+            <table class="table w-full">
+                <!-- head -->
+                <thead>
+                    <tr class="bg-base-200">
+                        <th>No</th>
+                        <th>Name</th>
 
-          <button class="bg-green-500 text-white w-32 h-8 my-2 mx-4 rounded-md hover:bg-green-700">Add Patient</button>
-        </a>
-      </div>
+                        <th>Date</th>
+                        <th>Gender</th>
+                        <th>Age</th>
+                        <th>Registration Number</th>
+                        <th>Care Number</th>
+                        <th>Unit</th>
+                        <th>Doctor</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- row 1 -->
+                    <tr v-for="(user, index) in users">
+                        <th>{{ index + 1 }}</th>
+                        <td>{{ user.name }}</td>
 
-        <div class="overflow-x-auto ">
-       <table class="table w-full">
-         <!-- head -->
-         <thead>
-           <tr class="bg-base-200">
-             <th>No</th>
-             <th>Name</th>
-             
-             <th>Date</th>
-             <th>Gender</th>
-             <th>Age</th>
-             <th>Registration Number</th>
-             <th>Care Number</th>
-             <th>Unit</th>
-             <th>Doctor</th>
-             <th>Actions</th>
-           </tr>
-         </thead>
-         <tbody>
-           <!-- row 1 -->
-           <tr v-for="user in users">
-             <th>{{  user.id }}</th>
-             <td>{{ user.name }}</td>
-             
-             <td>{{ user.created_at }}</td>
-             <td>{{ user.gender }}</td>
-             <td>{{ user.age }}</td>
-             <td>{{ user.no_reg }}</td>
-             <td>{{ user.no_rawat }}</td>
-             <td>{{ user.unit }}</td>
-             <td>{{ user.dr }}</td>
-             <td class="p-2">
-              <Link :href="`users/${user.id}/edit`">
-              
-                <button class="w-24 h-8 bg-blue-500 hover:bg-blue-700 mx-2  my-2 md:my-0  rounded-md" >Edit</button>
-              </Link>
-              <Link :href="`users/${user.id}`" method="delete">
-              
-                <button class="w-24 h-8 bg-red-700 hover:bg-red-800 mx-2 rounded-md" >Delete</button>
-              </Link>
-
-             </td>
-             
-           </tr>
-          
-         </tbody>
-       </table>
-     </div>
+                        <td>{{ user.created_at }}</td>
+                        <td>{{ user.gender }}</td>
+                        <td>{{ user.age }}</td>
+                        <td>{{ user.no_reg }}</td>
+                        <td>{{ user.no_rawat }}</td>
+                        <td>{{ user.unit }}</td>
+                        <td>{{ user.dr }}</td>
+                        <td class="p-2 flex flex-row text-white">
+                            <Link :href="`users/${user.id}/edit`">
+                                <button
+                                    class="w-24 h-8 bg-blue-500 hover:bg-blue-700 mx-2 my-2 md:my-0 rounded-md"
+                                >
+                                    🖋️ Edit
+                                </button>
+                            </Link>
+                            <Link :href="`users/${user.id}`" method="delete">
+                                <button
+                                    class="w-24 h-8 bg-red-700 hover:bg-red-800 mx-2 rounded-md"
+                                >
+                                   ❌ Delete
+                                </button>
+                            </Link>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
